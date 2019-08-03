8d5fd6e733d38b53afac056634a48df0190ee61a
Dan Seaman <danseaman6@gmail.com>
2019-08-03 12:28:53 -0400

    added favicon

--- server.py
+++ server.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+
 app = Flask(__name__, template_folder="public")
 
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 if __name__ == "__main__":
